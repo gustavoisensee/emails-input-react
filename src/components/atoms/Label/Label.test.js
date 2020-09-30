1898519a9c7339b1767e2f2a2d974011f3251199
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-09-30 23:22:52 +0200

    test: update and add new tests for label component

--- src/components/atoms/Label/Label.test.js
+++ src/components/atoms/Label/Label.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Label from './Label';
 
-test('renders component properly', () => {
-  const { getByText } = render(<Label text='test' />);
-  const text = getByText(/test/i);
+describe('<Label />', () => {
+  it('renders component properly', () => {
+    const { getByText } = render(<Label text='test' />);
+    const textElement = getByText(/test/i);
+    const deleteElement = getByText('x');
 
-  expect(text).toBeInTheDocument();
+    expect(textElement).toBeInTheDocument();
+    expect(deleteElement).toBeInTheDocument();
+  });
+
+  it('should call onDeleteClick when X is clicked', () => {
+    const onDeleteClick = jest.fn();
+    const { getByText } = render(<Label text='test' onDeleteClick={onDeleteClick} />);
+    const deleteButton = getByText('x');
+
+    fireEvent.click(deleteButton);
+
+    expect(onDeleteClick).toHaveBeenCalled();
+  });
 });

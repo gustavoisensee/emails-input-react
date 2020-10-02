78e19b4e793a51535d32bb30c9feb4ac3a04da76
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-10-02 18:10:32 +0200

    feat: add tests for emailEditor helper

---
+++ src/helpers/emailEditor.test.js
@@ -0,0 +1,42 @@
+import { COMMA, ENTER, KEY_COMMAND, KEY_V } from '../constants/emailEditor';
+import { handleEmailChecks } from './emailEditor';
+
+describe('emailEditor', () => {
+  it('should add the new email when Comma or Enter is pressed', () => {
+    const email = 't@miro.com,';
+    const emailsSubstring = email.substring(0, email.length - 1);
+    let emails = [{ id: 1, text: 'test@miro.com', valid: true }];
+    const setEmails = (_emails) => { emails = _emails };
+
+    handleEmailChecks({ keyCode: COMMA, target: { value: email } }, emails, setEmails);
+    expect(emails).toHaveLength(2);
+
+    const newEmail = emails.find(e => e.text === emailsSubstring);
+    expect(newEmail).toBeTruthy();
+
+    handleEmailChecks({ keyCode: ENTER, target: { value: email } }, emails, setEmails);
+    expect(emails).toHaveLength(3);
+  });
+
+  it('should add the new email(s) when copy and paste', () => {
+    const email = 'test,t@miro.com';
+    let emails = [{ id: 1, text: 'test@miro.com', valid: true }];
+    const setEmails = (_emails) => { emails = _emails };
+
+    handleEmailChecks({
+      keyCode: KEY_COMMAND, target: { value: email }, key: 'Meta'
+    }, emails, setEmails);
+    expect(emails).toHaveLength(3);
+
+    const newEmail = emails.find(e => e.text === 't@miro.com' && !!e.valid);
+    expect(newEmail).toBeTruthy();
+
+    const invalidEmail = emails.find(e => e.text === 'test' && e.valid === false);
+    expect(invalidEmail).toBeTruthy();
+
+    handleEmailChecks({
+      keyCode: KEY_V, target: { value: email }, ctrlKey: true
+    }, emails, setEmails);
+    expect(emails).toHaveLength(5);
+  });
+});
